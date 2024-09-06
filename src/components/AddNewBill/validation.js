75da4e633415383c3a2498bec8de3a0242c59f44
Jatinjohar17 <69750490+Jatinjohar17@users.noreply.github.com>
2024-09-06 14:55:55 +0530

    Update validation.js

--- src/components/AddNewBill/validation.js
+++ src/components/AddNewBill/validation.js
@@ -8,9 +8,7 @@ export const validateForm = (billName, billCategory, amount, dueDate, reminderFr
     if (amount <= 0 || isNaN(amount)) {
       return 'Amount must be a positive number.';
     }
-    if (new Date(dueDate) <= new Date()) {
-      return 'Due date must be a future date.';
-    }
+    
     if (!reminderFrequency) {
       return 'Please select a reminder frequency.';
     }
